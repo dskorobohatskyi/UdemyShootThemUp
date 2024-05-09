fa01c7fb34ea0944dd1da79fb2e72457cbbc05b7
Dmytro Skorobohatskyi <skorobogatskiidima@gmail.com>
2024-05-09 13:57:15 +0300

    Created pickup classes with basic logic and BPs for them

---
+++ Source/ShootThemUp/Public/Pickups/STUBasePickup.h
@@ -0,0 +1,31 @@
+// Shoot Them Up Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUBasePickup.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUBasePickup : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    ASTUBasePickup();
+
+protected:
+    UPROPERTY(VisibleAnywhere, Category = "Pickup")
+    USphereComponent* CollisionComponent;
+
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+    virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+};
